64a89053e43ea9d11ff8b8ef5312f69e19ebebde
phrpedroo <phrpachec@gmail.com>
2025-09-09 14:01:58 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const transport = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: 'phrpachec@gmail.com',
-        pass: 'guirozedsmxmdwlu',
+        user: 'seuemail@oqueiraenviar',
+        pass: 'sua-senha-via-app',
     }
 });
 
 transport.sendMail({
-    from: '<phrpachec@gmail.com> ',
-    to: 'danton.pietro@gmail.com',
+    from: '<seuemail@oqueiraenviar> ',
+    to: 'emaildequem@vaireceber',
     subject: 'Enviando email com Nodemailer',
     html: '<h1>esse email foi enviado por nodeMailer</h1>',
     text: 'esse email foi enviado por nodeMailer'
